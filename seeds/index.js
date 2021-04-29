1c4737fdfca8418a13e68fea09d26173207cbb66
Anshulsingh4 <anshul.chouhan@bigsteptech.com>
2021-04-29 18:41:17 +0530

    Update Commit

--- seeds/index.js
+++ seeds/index.js
@@ -2,14 +2,17 @@ const mongoose = require('mongoose');
 const cities = require('./cities');
 const { places, descriptors } = require('./seedHelpers');
 const Campground = require('../models/campground');
-const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp'
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config();
+}
+
+const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp';
 
 mongoose.connect(dbUrl, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology: true
 });
-
 const db = mongoose.connection;
 
 db.on("error", console.error.bind(console, "connection error:"));
